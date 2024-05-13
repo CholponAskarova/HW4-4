c52c292d912d85992b3ba2e213be287502c8176d
CholponAskarova <cholpontalantovna@gmail.com>
2024-05-13 19:33:50 +0600

    Initial Commit

---
+++ TableWiev1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableWiev1
+//
+//  Created by Cholpon on 13/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
